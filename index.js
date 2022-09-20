65f1dd2da85da0e094ded3823682d5b1e940c466
jb-abhi <jbabhi2@gmail.com>
2022-09-20 21:10:59 +0530

    auth

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
+const cors = require("cors");
 
 dotenv.config();
-app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -15,9 +15,13 @@ mongoose
     console.log(err);
   });
 
-app.use("api/user",userRoute);
-app.use("api/auth",authRoute);
-
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({
+  extended:true
+  }));
+app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
 
 
 app.listen(process.env.port || 5000, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,40 @@
-const router = require('express').Router();
-const User = require('../models/User')
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
-
-
-
-router.post("/register" ,async (req,res)=>{
+router.post("/register", async (req, res) => {
   const newUser = new User({
-    username:req.body.username,
-    email:req.body.email,
-    password:req.body.password
-  })
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD
+    ).toString(),
+  });
 
-  try{
+  try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-  }catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!");
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD
+    );
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    password !== req.body.password && res.status(401).json("Wrong Credentials");
+
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
