6cc37c5f593187bea357d08273d65991fdc8ca0a
jb-abhi <jbabhi2@gmail.com>
2022-09-20 14:19:19 +0530

    routes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('lets begin');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected Successfully"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("api/user",userRoute);
+app.use("api/auth",authRoute);
+
+
+
+app.listen(process.env.port || 5000, () => {
+  console.log("Server is listening");
+});
